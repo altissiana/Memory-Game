3b42054ed544bdf2afbb5794824fcc9a39b89871
Rendon De La Paz <rendondlp@gmail.com>
2019-04-04 16:22:39 -0700

    added shuffle feature

--- main.js
+++ main.js
@@ -16,7 +16,29 @@ var card9 = `<img src="assets/card-9.jpg"  class="elf" value="1">`
 var dankArr = [card1, card2, card3, card4, card5, card6, card7, card8, card9,
     card1, card2, card3, card4, card5, card6, card7, card8, card9];
 
-var randomize = Math.floor(Math.random()*dankArr.length);
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+  var newArr = shuffle(dankArr);
+  console.log(newArr);
+
+// var randomize = Math.floor(Math.random()*dankArr.length);
 
 
 
@@ -28,24 +50,23 @@ $(this).on('click', function(a){
     
 })
 
-
-back1.innerHTML = dankArr[randomize];
-back2.innerHTML = dankArr[randomize];
-back3.innerHTML = dankArr[randomize];
-back4.innerHTML = dankArr[randomize];
-back5.innerHTML = dankArr[randomize];
-back6.innerHTML = dankArr[randomize];
-back7.innerHTML = dankArr[randomize];
-back8.innerHTML = dankArr[randomize];
-back9.innerHTML = dankArr[randomize];
-back10.innerHTML = dankArr[randomize];
-back11.innerHTML = dankArr[randomize];
-back12.innerHTML = dankArr[randomize];
-back13.innerHTML = dankArr[randomize];
-back14.innerHTML = dankArr[randomize];
-back15.innerHTML = dankArr[randomize];
-back16.innerHTML = dankArr[randomize];
-back17.innerHTML = dankArr[randomize];
-back18.innerHTML = dankArr[randomize];
+back1.innerHTML = newArr[0];
+back2.innerHTML = newArr[1];
+back3.innerHTML = newArr[2];
+back4.innerHTML = newArr[3];
+back5.innerHTML = newArr[4];
+back6.innerHTML = newArr[5];
+back7.innerHTML = newArr[6];
+back8.innerHTML = newArr[7];
+back9.innerHTML = newArr[8];
+back10.innerHTML = newArr[9];
+back11.innerHTML = newArr[10];
+back12.innerHTML = newArr[11];
+back13.innerHTML = newArr[12];
+back14.innerHTML = newArr[13];
+back15.innerHTML = newArr[14];
+back16.innerHTML = newArr[15];
+back17.innerHTML = newArr[16];
+back18.innerHTML = newArr[17];
 
 
