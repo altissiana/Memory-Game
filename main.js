03d8035ae3f7208b0587a47f0c71b24739e250fa
Rendon De La Paz <rendondlp@gmail.com>
2019-04-04 14:02:59 -0700

    added stuff

---
+++ main.js
@@ -0,0 +1,21 @@
+
+$('img').on('click', function(){
+    $(this).addClass('show')
+})
+
+$(".card").flip();
+
+var checkArr = [];
+
+var card1 = `<img src="assets/card-1.jpeg" class="cena" value="1"></img>`
+var card2= `<img src="assets/card-2.jpg" class="jimmy" value="2"></img>`
+
+
+
+back1.innerHTML = card1
+back2.innerHTML = card1
+back3.innerHTML = card2
+back4.innerHTML = card2
+
+
+
