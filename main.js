34e428f086f9afb6dae4fab7e62a6272f1f7552c
Rendon De La Paz <rendondlp@gmail.com>
2019-04-04 15:42:21 -0700

    added dank stuff

--- main.js
+++ main.js
@@ -1,21 +1,51 @@
 
-$('img').on('click', function(){
-    $(this).addClass('show')
-})
-
-$(".card").flip();
 
 var checkArr = [];
 
-var card1 = `<img src="assets/card-1.jpeg" class="cena" value="1"></img>`
-var card2= `<img src="assets/card-2.jpg" class="jimmy" value="2"></img>`
+var card1 = `<img src="assets/card-1.jpeg" class="cena" value="1">`
+var card2 = `<img src="assets/card-2.jpg"  class="jimmy" value="2">`
+var card3 = `<img src="assets/card-3.jpg"  class="jerry" value="1">`
+var card4 = `<img src="assets/card-4.jpeg" class="catface" value="2">`
+var card5 = `<img src="assets/card-5.jpg"  class="hank" value="1">`
+var card6 = `<img src="assets/card-6.jpg"  class="bee" value="2">`
+var card7 = `<img src="assets/card-7.jpg"  class="spongebob" value="1">`
+var card8 = `<img src="assets/card-8.jpeg" class="satan" value="2">`
+var card9 = `<img src="assets/card-9.jpg"  class="elf" value="1">`
+
+
+var dankArr = [card1, card2, card3, card4, card5, card6, card7, card8, card9,
+    card1, card2, card3, card4, card5, card6, card7, card8, card9];
+
+var randomize = Math.floor(Math.random()*dankArr.length);
 
 
 
-back1.innerHTML = card1
-back2.innerHTML = card1
-back3.innerHTML = card2
-back4.innerHTML = card2
+$(this).on('click', function(a){
+    
+    $('.card').flip()
+    console.log($(a.target).attr('value'))
+    // console.log($(b.target).attr('value'))
+    
+})
 
+
+back1.innerHTML = dankArr[randomize];
+back2.innerHTML = dankArr[randomize];
+back3.innerHTML = dankArr[randomize];
+back4.innerHTML = dankArr[randomize];
+back5.innerHTML = dankArr[randomize];
+back6.innerHTML = dankArr[randomize];
+back7.innerHTML = dankArr[randomize];
+back8.innerHTML = dankArr[randomize];
+back9.innerHTML = dankArr[randomize];
+back10.innerHTML = dankArr[randomize];
+back11.innerHTML = dankArr[randomize];
+back12.innerHTML = dankArr[randomize];
+back13.innerHTML = dankArr[randomize];
+back14.innerHTML = dankArr[randomize];
+back15.innerHTML = dankArr[randomize];
+back16.innerHTML = dankArr[randomize];
+back17.innerHTML = dankArr[randomize];
+back18.innerHTML = dankArr[randomize];
 
 
